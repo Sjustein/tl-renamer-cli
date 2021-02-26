e717ffc884942a82b79643d3f507e70b0844b2f8
Justin Ruiter | s2264609 <j.a.g.ruiter@student.utwente.nl>
2021-02-26 18:55:54 +0100

    Initial commit - cmake project setup

---
+++ tl-renamer-cli/tl-renamer-cli.cpp
@@ -0,0 +1,12 @@
+﻿// tl-renamer-cli.cpp : Defines the entry point for the application.
+//
+
+#include "tl-renamer-cli.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ tl-renamer-cli/tl-renamer-cli.h
@@ -0,0 +1,8 @@
+﻿// tl-renamer-cli.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
