edac4b033a7637d848ee43f7c15d1a563a1fa133
Justin Ruiter | s2264609 <j.a.g.ruiter@student.utwente.nl>
2021-03-05 10:03:26 +0100

    Help + argument validation

--- tl-renamer-cli/tl-renamer-cli.cpp
+++ tl-renamer-cli/tl-renamer-cli.cpp
@@ -12,7 +12,8 @@ namespace fs = std::filesystem;
 	#define PATH_SEP '/'
 #endif
 
-void processDirectory(std::string dir) {
+void processDirectory(std::string dir)
+{
 	std::map<unsigned long, std::string> files;
 
 	// Gather the file creation time of files
@@ -24,28 +25,48 @@ void processDirectory(std::string dir) {
 	}
 
 	// The dictionary is already ordered, check if the converted directory exists
-	fs::directory_entry convertedPath(dir + "Converted");
+	fs::directory_entry convertedPath(dir + "Processed");
 	if (convertedPath.exists())
 		std::filesystem::remove_all(convertedPath.path());
 
 	std::filesystem::create_directory(convertedPath.path());
 
 	int i = 0;
-	std::string dirBase = dir + "Converted" + PATH_SEP;
+	std::string dirBase = dir + "Processed" + PATH_SEP;
 	for (const auto& cur : files) {
 		fs::directory_entry curFile(dir + cur.second);
-		std::string curFile = dirBase + std::to_string(i) + curFile.path().extension().string();
-		fs::directory_entry toPath(curFile);
+		std::string curFilePath = dirBase + std::to_string(i) + curFile.path().extension().string();
+		fs::directory_entry toPath(curFilePath);
 
 		std::filesystem::copy(curFile.path(), toPath.path());
-		std::cout << cur.second << " -> " << curFile << std::endl;
+		std::cout << cur.second << " -> " << curFilePath << std::endl;
 
 		i++;
 	}
 }
 
+bool validateArgs(int argc, char* argv[])
+{
+	return false;
+	if (argc < 3)
+		return true;
+
+	return argv[2] != "-m";
+}
+
+void printHelp() {
+	std::cout << "USAGE: tl-renamer-cli path_to_proces [arguments]" << std::endl <<std::endl
+				<< "-m " << std::setw(75) << "Move the files to the processed directory, instead of copying them" << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
+	// Check if the arguments are valid
+	if (!validateArgs(argc, argv)) {
+		std::cout << "The specified command line arguments are not in the correct format." << std::endl << std::endl;
+		printHelp();
+	}
+
 	// Check if the directory has been specified in a cli argument
 	std::string dir;
 	if (argc > 1)
